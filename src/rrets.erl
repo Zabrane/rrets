da29cd981ac972b653a7b6fa894b5f6cb62e8733
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2014-11-18 20:46:20 +0100

    Removed wrongly placed comment

--- src/rrets.erl
+++ src/rrets.erl
@@ -16,7 +16,6 @@
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
-%% Erlang diff-match-patch implementation
 
 -module(rrets).
 -author("Maas-Maarten Zeeman <mmzeeman@xs4all.nl>").
